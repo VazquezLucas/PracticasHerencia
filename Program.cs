ba4091955e269775ca10db776c0c708f8d803dda
Lucas Vazquez <vazquezlucasmatias2001@gmail.com>
2023-03-19 19:41:23 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace PracticasHerencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Caballo miBabieca=new Caballo("babieca");
+
+            Humano miJuan = new Humano("Juan");
+
+            Gorila miCopito = new Gorila("Copito");
+
+            miJuan.getNombre();
+
+            miBabieca.getNombre();
+
+            miCopito.getNombre();
+
+        }
+    }
+
+    class Mamiferos:Object
+    {
+        public Mamiferos(String nombre) 
+        {
+
+            nombreSerVivo = nombre;
+
+        }
+
+        public void respirar()
+        {
+            Console.WriteLine("Soy capaz de respirar");
+        }
+
+        public void cuidarCrias()
+        {
+            Console.WriteLine("Cuido de mis crias hasta que se valgan por si mismas");
+        }
+
+        public void getNombre()
+        {
+            Console.WriteLine("El nombre del ser vivo es: " + nombreSerVivo);
+        } 
+
+        private String nombreSerVivo;
+    }
+
+    class Caballo : Mamiferos
+    {
+
+        public Caballo(String nombreCaballo) : base(nombreCaballo)
+        {
+
+
+
+        }
+
+        public void galopar()
+        {
+            Console.WriteLine("Soy capaz de galopar");
+        }
+    }
+
+    class Humano : Mamiferos
+    {
+        public Humano(String nombreHumano) : base(nombreHumano)
+        {
+
+
+
+        }
+        public void pensar()
+        {
+            Console.WriteLine("Soy capaz de pensar ¿?");
+        }
+    }
+
+    class Gorila : Mamiferos
+    {
+        public Gorila(String nombreGorila) : base(nombreGorila)
+        {
+
+
+
+        }
+        public void trepar()
+        {
+            Console.WriteLine("Soy capaz de trepar");
+        }
+    }
+}
